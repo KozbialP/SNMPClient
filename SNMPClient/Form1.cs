f7c0706c7592872fb7c27d686738ef33c17cddb1
Paweł Koźbiał <pawel2851@gmail.com>
2016-12-21 15:21:06 +0100

    Functionality: -get -getNext -getTable -monitorOID

--- SNMPClient/Form1.cs
+++ SNMPClient/Form1.cs
@@ -7,14 +7,223 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SnmpSharpNet;
+using System.Net;
+using System.IO;
+using System.Timers;
 
 namespace SNMPClient
 {
     public partial class Form1 : Form
     {
+        Request request;
         public Form1()
         {
+            request = new Request();
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SnmpV1Packet res = new SnmpV1Packet();
+            res = request.Get(textBox1.Text);
+            if (res != null)
+            {
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.RowTemplate.Clone();
+                row.CreateCells(dataGridView1, res.Pdu.VbList[0].Oid.ToString(), res.Pdu.VbList[0].Value.ToString(), SnmpConstants.GetTypeName(res.Pdu.VbList[0].Value.Type), "localhost");
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SnmpV1Packet res = new SnmpV1Packet();
+            res = request.GetNext(textBox1.Text, textBox2.Text);
+            if (res != null)
+            {
+                foreach (Vb v in res.Pdu.VbList)
+                {
+                    DataGridViewRow row = (DataGridViewRow)dataGridView1.RowTemplate.Clone();
+                    row.CreateCells(dataGridView1, res.Pdu.VbList[0].Oid.ToString(), res.Pdu.VbList[0].Value.ToString(), SnmpConstants.GetTypeName(res.Pdu.VbList[0].Value.Type), "localhost");
+                    dataGridView1.Rows.Add(row);
+                }
+                textBox2.Clear();
+                textBox2.AppendText(res.Pdu.VbList[0].Oid.ToString());
+                textBox2.Update();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string fileLogPath = textBox3.Text + ".txt";
+            Dictionary<String, Dictionary<uint, AsnType>> result = new Dictionary<String, Dictionary<uint, AsnType>>();
+            // Not every row has a value for every column so keep track of all columns available in the table
+            List<uint> tableColumns = new List<uint>();
+            List<Oid> columns = new List<Oid>();
+            OctetString community = new OctetString("public");
+            IpAddress peer = new IpAddress("localhost");
+            AgentParameters param = new AgentParameters(community);
+            UdpTarget target = new UdpTarget((IPAddress)peer);
+            Oid startOid = new Oid(textBox1.Text);
+            startOid.Add(1);
+            Pdu pdu = new  Pdu(PduType.GetNext);
+            pdu.VbList.Add(startOid);
+            Oid curOid = (Oid)startOid.Clone();
+            while (startOid.IsRootOf(curOid))
+            {
+                SnmpV1Packet res;
+                try
+                {
+                    res = (SnmpV1Packet)target.Request(pdu, param);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Request failed: {0}", ex.Message);
+                    target.Close();
+                    return;
+                }
+                foreach (Vb v in res.Pdu.VbList)
+                {
+                    curOid = (Oid)v.Oid.Clone();
+                    if (startOid.IsRootOf(v.Oid))
+                    {
+                        // Get child Id's from the OID (past the table.entry sequence)
+                        uint[] childOids = Oid.GetChildIdentifiers(startOid, v.Oid);
+                        // Get the value instance and converted it to a dotted decimal
+                        //  string to use as key in result dictionary
+                        uint[] instance = new uint[childOids.Length - 1];
+                        Array.Copy(childOids, 1, instance, 0, childOids.Length - 1);
+                        String strInst = InstanceToString(instance);
+                        // Column id is the first value past <table oid>.entry in the response OID
+                        uint column = childOids[0];
+                        Oid leaf = (Oid)startOid.Clone();
+                        leaf.Add(column);
+                        if (!tableColumns.Contains(column))
+                            tableColumns.Add(column);
+                        if(!columns.Contains(leaf))
+                            columns.Add(leaf);
+                        if (result.ContainsKey(strInst))
+                        {
+                            result[strInst][column] = (AsnType)v.Value.Clone();
+                        }
+                        else
+                        {
+                            result[strInst] = new Dictionary<uint, AsnType>();
+                            result[strInst][column] = (AsnType)v.Value.Clone();
+                        }
+                    }
+                }
+                if (startOid.IsRootOf(curOid))
+                {
+                    pdu.VbList.Clear();
+                    pdu.VbList.Add(curOid);
+                }
+            }
+            if (result.Count <= 0)
+            {
+                MessageBox.Show("No results returned.\n");
+            }
+            else
+            {
+                StringBuilder header = new StringBuilder();
+                header.Append("Instance");
+                foreach (Oid column in columns)
+                {
+                    header.Append("\t" + column.ToString());
+                }
+                MakeLog(header.ToString(), fileLogPath);
+                foreach (KeyValuePair<string, Dictionary<uint, AsnType>> kvp in result)
+                {
+                    StringBuilder s = new StringBuilder();
+                    s.Append(kvp.Key);
+                    foreach (uint column in tableColumns)
+                    {
+                        if (kvp.Value.ContainsKey(column))
+                        {
+                            s.Append(" \t ");
+                            s.Append(kvp.Value[column].ToString());
+                            s.Append(" ");
+                            s.Append("(" + SnmpConstants.GetTypeName(kvp.Value[column].Type) + ")");
+                        }
+                        else
+                        {
+                            s.Append("\t-");
+                        }
+                    }
+                    MakeLog(s.ToString(), fileLogPath);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+        public static string InstanceToString(uint[] instance)
+        {
+            StringBuilder str = new StringBuilder();
+            foreach (uint v in instance)
+            {
+                if (str.Length == 0)
+                    str.Append(v);
+                else
+                    str.AppendFormat(".{0}", v);
+            }
+            return str.ToString();
+        }
+        public static void MakeLog(string logDescription, string fileLogPath)
+        {
+            using (StreamWriter file = new StreamWriter(fileLogPath, true))
+            {
+                file.WriteLine(logDescription);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(Monitor);
+            aTimer.Interval = 5000;
+            if (aTimer.Enabled)
+                aTimer.Stop();
+            else
+                aTimer.Start();
+        }
+        private void Monitor(object source, ElapsedEventArgs e)
+        {
+            string oid = textBox1.Text;
+            if (textBox2.Text == null)
+                textBox2.Text = oid;
+            string currentOid = textBox2.Text;
+            SnmpV1Packet res = new SnmpV1Packet(currentOid);
+            res = request.Get(oid);
+            if (res != null)
+            {
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.RowTemplate.Clone();
+                row.CreateCells(dataGridView1, res.Pdu.VbList[0].Oid.ToString(), res.Pdu.VbList[0].Value.ToString(), SnmpConstants.GetTypeName(res.Pdu.VbList[0].Value.Type), "localhost");
+                dataGridView1.Rows.Add(row);
+            }
         }
     }
 }
---
+++ SNMPClient/Request.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using SnmpSharpNet;
+using System.Windows.Forms;
+
+namespace SNMPClient
+{
+    class Request
+    {
+        OctetString community;
+        AgentParameters param;
+        string agentAddress;
+        IpAddress agent;
+        UdpTarget target;
+        Oid rootOid = new Oid();
+        Oid lastOid = new Oid();
+        public Request()
+        {
+            community = new OctetString("public");
+            param = new AgentParameters(community);
+            param.Version = SnmpVersion.Ver1;
+            agentAddress = "localhost";
+            Initialize();
+        }
+        public void Initialize()
+        {
+            agent = new IpAddress(agentAddress);
+            target = new UdpTarget((IPAddress)agent, 161, 2000, 1);
+        }
+        public SnmpV1Packet Get(string oid)
+        {
+            
+            Pdu pdu = new Pdu(PduType.Get);
+            pdu.VbList.Add(oid);
+
+            // Make SNMP request
+            SnmpV1Packet result = (SnmpV1Packet)target.Request(pdu, param);
+
+            // If result is null then agent didn't reply or we couldn't parse the reply.
+            if (result != null)
+            {
+                // ErrorStatus other then 0 is an error returned by 
+                // the Agent - see SnmpConstants for error definitions
+                if (result.Pdu.ErrorStatus != 0)
+                {
+                    // agent reported an error with the request
+                    MessageBox.Show("Error in SNMP reply. Error " + result.Pdu.ErrorStatus + " index " + result.Pdu.ErrorIndex);
+                    return null;
+                }
+                else
+                {
+                    // Reply variables are returned in the same order as they were added
+                    //  to the VbList
+                    return result;
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("No response received from SNMP agent.");
+                return null;
+            }
+        }
+        public SnmpV1Packet GetNext (string OID, string lastOID)
+        {
+            if (rootOid != null)
+            {
+                Oid tmp = new Oid(OID);
+                if(rootOid == tmp)
+                {
+                    lastOid = new Oid(lastOID);
+                }
+                else
+                {
+                    rootOid = tmp;
+                    lastOid = rootOid;
+                }
+            }
+            else
+            {
+                rootOid = new Oid(OID);
+                lastOid = rootOid;
+            }
+              Pdu pdu = new Pdu(PduType.GetNext);
+              pdu.VbList.Clear();
+              pdu.VbList.Add(lastOid);
+              SnmpV1Packet result = (SnmpV1Packet)target.Request(pdu, param);
+              if (result != null)
+              {
+                  // ErrorStatus other then 0 is an error returned by 
+                  // the Agent - see SnmpConstants for error definitions
+                  if (result.Pdu.ErrorStatus != 0)
+                  {
+                      // agent reported an error with the request
+                      Console.WriteLine("Error in SNMP reply. Error {0} index {1}",
+                          result.Pdu.ErrorStatus,
+                          result.Pdu.ErrorIndex);
+                      lastOid = null;
+                      return null;
+                  }
+                  else
+                  {
+                       Vb v = result.Pdu.VbList[0];
+                           // Check that retrieved Oid is "child" of the root OID
+                           if (rootOid.IsRootOf(v.Oid))
+                           {
+                               Console.WriteLine("{0} ({1}): {2}",
+                                   v.Oid.ToString(),
+                                   SnmpConstants.GetTypeName(v.Value.Type),
+                                   v.Value.ToString());
+                                   lastOid = v.Oid;
+                        return result;
+                           }
+                           else
+                           {
+                               // we have reached the end of the requested
+                               // MIB tree. Set lastOid to null and exit loop
+                               lastOid = null;
+                               return null;
+                           } 
+                    }
+              }
+              else
+              {
+                  Console.WriteLine("No response received from SNMP agent.");
+                  return null;
+              } 
+        }
+        public void GetTable()
+        {
+
+        }
+    }
+}
